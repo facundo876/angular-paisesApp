490d0f00081c0abaa10ffcd6285006b86334b6b3
Facundo Arce <40859125+facundo876@users.noreply.github.com>
2022-04-19 02:41:20 -0300

    Por region completado

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
+  styles: [`
+    button{
+      margin: 3px
+    }
+  `
   ]
 })
 export class PorRegionComponent implements OnInit {
 
-  constructor() { }
+  regiones: string[] = ['EU',
+  'EFTA',
+  'CARICOM',
+  'PA',
+  'AU',
+  'USAN',
+  'EEU',
+  'AL',
+  'ASEAN',
+  'CAIS',
+  'CEFTA',
+  'NAFTA',
+  'SAARC'];
+  regionActiva:string = '';
+  public hayError: boolean = false;
+  paises:Country[] = [];
+
+  constructor( private paisService: PaisService ) { }
+
+  getCssClass( region:string ): string{
+    return ( region === this.regionActiva) ? 'btn btn-primary' : 'btn btn-outline-primary';
+  }
 
   ngOnInit(): void {
+  }
+
+  activarRegion( region:string ){
+
+    if( region == this.regionActiva ) { return; }
+
+    this.regionActiva = region;
+    this.hayError = false;
+    
+    this.buscar( region ); 
+
+  }
+  
+  private buscar( region: string ){
+
+    this.paises = [];
+
+    this.paisService.buscarRegion( region )
+    .subscribe( paises => { this.paises = paises });
   }
 
 }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Country } from '../interfaces/pais.interface';
@@ -12,23 +12,34 @@ export class PaisService {
 
   constructor( private http: HttpClient ) { }
 
+  get httpParams(){
+    return new HttpParams().set( 'fields', 'name,capital,alpha2Code,flag,population' );
+  }
+
   buscarPais( termino:string ): Observable<Country[]>{
 
     const url = `${ this.apiUrl }/name/${ termino }`;
 
-    return this.http.get<Country[]>( url );
+    return this.http.get<Country[]>( url, { params: this.httpParams } );
   }
 
   buscarCapital( termino:string ): Observable<Country[]>{
     const url = `${ this.apiUrl }/capital/${ termino }`;
 
-    return this.http.get<Country[]>( url );
+    return this.http.get<Country[]>( url, { params: this.httpParams } );
   }
 
   getPaisPorAlpha( id:string ): Observable<Country>{
     const url = `${ this.apiUrl }/alpha/${ id }`;
 
     return this.http.get<Country>( url );
+  }
+
+  buscarRegion( region:string ): Observable<Country[]>{
+
+    const url = `${ this.apiUrl }/regionalbloc/${ region }`;
+
+    return this.http.get<Country[]>( url, { params: this.httpParams } );
   }
 
 }
